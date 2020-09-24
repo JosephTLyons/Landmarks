e241bdba008a91e7c453033cd4bc4a621f5f1e61
Joseph T Lyons <JosephTLyons@gmail.com>
2020-09-23 23:44:48 -0400

    Current work on Part 3

--- Part 3/HandlingUserInput/StartingPoint/Landmarks/Landmarks/LandmarkList.swift
+++ Part 3/HandlingUserInput/StartingPoint/Landmarks/Landmarks/LandmarkList.swift
@@ -8,18 +8,21 @@ A view showing a list of landmarks.
 import SwiftUI
 
 struct LandmarkList: View {
-    @State var showFavoritesOnly = true
+    @EnvironmentObject var userData: UserData
 
     var body: some View {
         NavigationView {
             List {
-                Toggle(isOn: $showFavoritesOnly) {
+                Toggle(isOn: $userData.showFavoritesOnly) {
                     Text("Favorites Only")
                 }
                 
                 ForEach(landmarkData) { landmark in
-                    if !self.showFavoritesOnly || landmark.isFavorite {
-                        NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    if !self.userData.showFavoritesOnly || landmark.isFavorite {
+                        NavigationLink(
+                            destination: LandmarkDetail(landmark: landmark)
+                                .environmentObject(self.userData)
+                        ) {
                             LandmarkRow(landmark: landmark)
                         }
                     }
@@ -37,5 +40,6 @@ struct LandmarkList_Previews: PreviewProvider {
                 .previewDevice(PreviewDevice(rawValue: deviceName))
                 .previewDisplayName(deviceName)
         }
+        .environmentObject(UserData())
     }
 }
